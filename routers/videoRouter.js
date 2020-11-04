721aafa70643a2ad1b815326db972f45678e2a94
bdgom <bdgom73@naver.com>
2020-11-04 16:28:20 +0900

    1104

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,14 +1,19 @@
 import express from "express"
-import { editVideo, getUpload, postUpload, upload, videoDetail } from "../controllers/videoController";
+import { getEditVideo, getUpload, postUpload, upload, videoDetail,postEditVideo, deleteVideo } from "../controllers/videoController";
 import { uploadVideo } from "../middlewares";
 import routes from "../routes"
 const videoRouter = express.Router();
 
 videoRouter.get("/",(req,res)=>{ res.send("videos") })
+
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo,postUpload);
+
 videoRouter.get(routes.videoDetail(),videoDetail)
-videoRouter.get(routes.editVideo,editVideo)
-videoRouter.get(routes.deleteVideo,(req,res)=>{ res.send("seadeleteVideorch") })
+
+videoRouter.get(routes.editVideo(), getEditVideo)
+videoRouter.post(routes.editVideo(), postEditVideo)
+
+videoRouter.get(routes.deleteVideo(),deleteVideo)
 
 export default videoRouter;
