929417a87497f119ca8cf0d627359da59bb7883f
bdgom <bdgom73@naver.com>
2020-11-20 23:44:40 +0900

    mongo plus

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 mongoose.connect(
-    process.env.MONGO_URL_PROD,
+    process.env.MONGO,
     {
         useNewUrlParser : true,
         useFindAndModify : false,
