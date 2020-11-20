c0bdcd2329819e740cf4b8a61d8d60f8ea4db007
bdgom <bdgom73@naver.com>
2020-11-20 19:38:44 +0900

    2323

--- src/init.js
+++ src/init.js
@@ -8,12 +8,11 @@ import app from "./app";
 import "./models/Video"
 import "./models/Comment"
 import "./models/User"
-const port = 3010;
-
+const host = '0.0.0.0';
 const port = process.env.PORT || 3010;
 
 const handleListening = ()=>{
     console.log(`Listening on : port ${port}`)
 }
 
-app.listen(port,handleListening)
+app.listen(port,host,handleListening)
